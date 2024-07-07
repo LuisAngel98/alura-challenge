fb4179b05bf8434fc6292a3f85d53dd3d005b670
LuisAngel98 <luis37813@gmail.com>
2024-07-07 18:42:08 -0500

    Refactored repetitive lines and added functionality to copy result text

--- js/index.js
+++ js/index.js
@@ -1,24 +1,27 @@
-import { encrypt, decrypt } from "./utils.js";
+import { encrypt, decrypt, validateInput } from "./utils.js";
 
 //variables de los elementos del html
 const inputText = document.getElementById("inputText");
 const resultText = document.getElementById("resultText");
 const btnEncrypt = document.getElementById("btnEncrypt");
 const btnDecrypt = document.getElementById("btnDecrypt");
+const btnCopy = document.getElementById("btnCopy");
 //evento
 btnEncrypt.addEventListener("click", () => {
-  const containerWarning = document.getElementById("container-warning");
-  const containerResult = document.getElementById("container-result");
-  // Mostrar el mensaje "Ningún mensaje fue encontrado" si el texto esta vacio
-  if (inputText.value.trim() == "") {
-    containerWarning.classList.remove("hidden");
-    containerResult.classList.add("hidden");
-    return;
-  }
-  containerWarning.classList.add("hidden");
-  containerResult.classList.remove("hidden");
-  resultText.value = encrypt(inputText.value);
+  validateInput(inputText.value.trim());
+  resultText.value = encrypt(inputText.value.trim());
 });
 btnDecrypt.addEventListener("click", () => {
-  console.log(decrypt(inputText));
+  validateInput(inputText.value.trim());
+  resultText.value = decrypt(inputText.value.trim());
+});
+btnCopy.addEventListener("click", async () => {
+  resultText.select();
+  resultText.setSelectionRange(0, 99999);
+  try {
+    await navigator.clipboard.writeText(resultText.value);
+    alert("Texto copiado: " + resultText.value);
+  } catch (err) {
+    console.error("Error al copiar el texto: ", err);
+  }
 });
--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,19 @@
 function encrypt(data) {
-  return data;
+  return data + " encrypt";
 }
 function decrypt(encryptedData) {
-  return encryptedData;
+  return encryptedData + " decrypt";
 }
-export { encrypt, decrypt };
+function validateInput(inputText) {
+  const containerWarning = document.getElementById("container-warning");
+  const containerResult = document.getElementById("container-result");
+  // Mostrar el mensaje "Ningún mensaje fue encontrado" si el texto esta vacio
+  if (inputText == "") {
+    containerWarning.classList.remove("hidden");
+    containerResult.classList.add("hidden");
+    return;
+  }
+  containerWarning.classList.add("hidden");
+  containerResult.classList.remove("hidden");
+}
+export { encrypt, decrypt, validateInput };
